086e4c1e41b34ed65efd4941214b912b11ae004e
Brian Choi <b.choi562@gmail.com>
2021-02-12 09:28:29 -0800

    sprint challenge mvp

--- api/server.js
+++ api/server.js
@@ -3,5 +3,21 @@ const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
+
+server.use(express.json())
+
+function logger(req, res, next){
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+    next();
+}
+
+server.use(logger)
+server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
+server.get('/', (req, res) => {
+   res.send(`<h2>Server Working!</h2>`)
+});
 
 module.exports = server;
